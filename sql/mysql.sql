f9372ace8327d93d289740ef161e85bff4168335
Kristian Lyngstol <kristian@evo.(none)>
2007-03-12 16:11:01 +0100

    Update the mysql user table.

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,13 +1,25 @@
--- MySQL dump 9.11
+-- MySQL dump 10.11
 --
 -- Host: localhost    Database: bwreg2
 -- ------------------------------------------------------
--- Server version	4.0.24_Debian-10sarge2-log
+-- Server version	5.0.32-Debian_7-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `uid` int(11) NOT NULL auto_increment,
   `uname` varchar(12) NOT NULL default '',
@@ -18,7 +30,18 @@ CREATE TABLE `users` (
   `adress` varchar(40) NOT NULL default '',
   `phone` varchar(12) NOT NULL default '',
   `extra` varchar(50) default NULL,
-  `pass` varchar(20) default NULL,
-  PRIMARY KEY  (`uid`)
-) TYPE=MyISAM;
+  `pass` blob NOT NULL,
+  PRIMARY KEY  (`uid`),
+  KEY `names` (`lastname`,`firstname`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2007-03-12 15:10:30
