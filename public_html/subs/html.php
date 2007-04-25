46f0f99321e8f5e9baaf3cf6943cf40016677776
Kristian Lyngstol <kristian@evo.(none)>
2007-04-25 15:55:53 +0200

    Fix copyright in header.....

--- public_html/subs/html.php
+++ public_html/subs/html.php
@@ -1,7 +1,7 @@
 <?
 
 /* BWReg2 HTML rendering classes
- * Copyright (C) yyyy  name of author
+ * Copyright (C) 2007 Kristian Lyngstøl
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
